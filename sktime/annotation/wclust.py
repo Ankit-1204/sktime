0d1078430d6db89662d7404a1820180fffcf280a
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-10-11 01:07:10 +0530

    [ENH] Overlapping Segmenter Implement, Aggregator implemented

--- sktime/annotation/wclust.py
+++ sktime/annotation/wclust.py
@@ -7,13 +7,33 @@ Implementing segmentation using clustering, Read more at
 import numpy as np
 import pandas as pd
 from sklearn.base import clone
-from sklearn.cluster import KMeans
 
 from sktime.annotation.base import BaseSeriesAnnotator
+from sktime.clustering.k_means import TimeSeriesKMeans
 from sktime.utils.sklearn import is_sklearn_clusterer
 
 __author__ = ["Ankit-1204"]
 __all__ = ["WindowSegmenter"]
+
+
+def overlapping_window(window_size, step_size, X):
+    X_size = len(X)
+    n_features = X.shape[1]
+
+    sub_seg = []
+
+    for i in range(0, X_size, step_size):
+        end_idx = i + window_size
+        segment = X.iloc[i:end_idx].values
+
+        if len(segment) < window_size:
+            pad_length = window_size - len(segment)
+            pad = np.zeros((pad_length, n_features))
+            segment = np.concatenate([segment, pad], axis=0)
+
+        sub_seg.append(segment)
+
+    return np.array(sub_seg).reshape(len(sub_seg), n_features, window_size)
 
 
 def window_timeseries(window_size, X):
@@ -98,6 +118,19 @@ def finalLabels(labels, window_size, X):
     return np.array(flabel)
 
 
+def overlap_final_label(labels, window_size, step_size, X):
+    time_point_labels = [[] for _ in range(len(X))]
+
+    for i in range(len(labels)):
+        start_ind = i * step_size
+        end_ind = start_ind + window_size
+        if end_ind > len(X):
+            end_ind = len(X)
+        for j in range(start_ind, end_ind):
+            time_point_labels[j].append(int(labels[i]))
+    return time_point_labels
+
+
 class WindowSegmenter(BaseSeriesAnnotator):
     """Window-based Time Series Segmentation via Clustering.
 
@@ -119,7 +152,7 @@ class WindowSegmenter(BaseSeriesAnnotator):
     >>> from sktime.annotation.wclust import WindowSegmenter
     >>> from sktime.datasets import load_gunpoint
     >>> X, y = load_gunpoint()
-    >>> clusterer = KMeans()
+    >>> clusterer = TimeSeriesKMeans()
     >>> segmenter = ClusterSegmenter(clusterer, 3)
     >>> segmenter._fit(X)
     >>> segment_labels = segmenter._predict(X)
@@ -130,13 +163,15 @@ class WindowSegmenter(BaseSeriesAnnotator):
         "learning_type": "unsupervised",
     }
 
-    def __init__(self, clusterer=None, window_size=1):
+    def __init__(self, clusterer=None, window_size=1, overlap=False, step_size=1):
         self.clusterer = clusterer
         self._clusterer_ = clusterer
         self.window_size = window_size
         self._window_size = window_size
+        self.overlap = overlap
+        self.step_size = step_size
         if self.clusterer is None:
-            self._clusterer = KMeans()
+            self._clusterer = TimeSeriesKMeans()
         else:
             self._clusterer = self.clusterer
         super().__init__()
@@ -163,17 +198,23 @@ class WindowSegmenter(BaseSeriesAnnotator):
         """
         if isinstance(X, pd.Series):
             X = X.to_frame(X)
-        if is_sklearn_clusterer(self._clusterer):
-            win_x = window(self._window_size, X)
-            seg = flattenSegments(win_x)
-            cloned_clusterer = clone(self._clusterer)
-            cloned_clusterer.fit(seg)
-        else:
-            win_x = window_timeseries(self._window_size, X)
+        if self.overlap:
+            win_x = overlapping_window(self._window_size, self.step_size, X)
             cloned_clusterer = clone(self._clusterer)
             cloned_clusterer.fit(win_x)
+            self._clusterer_ = cloned_clusterer
+        else:
+            if is_sklearn_clusterer(self._clusterer):
+                win_x = window(self._window_size, X)
+                seg = flattenSegments(win_x)
+                cloned_clusterer = clone(self._clusterer)
+                cloned_clusterer.fit(seg)
+            else:
+                win_x = window_timeseries(self._window_size, X)
+                cloned_clusterer = clone(self._clusterer)
+                cloned_clusterer.fit(win_x)
+            self._clusterer_ = cloned_clusterer
 
-        self._clusterer_ = cloned_clusterer
         return self
 
     def _predict(self, X):
@@ -194,15 +235,23 @@ class WindowSegmenter(BaseSeriesAnnotator):
             X = X.to_frame(X)
 
         self.n_features, self.n_timepoints = X.shape
-        if is_sklearn_clusterer(self._clusterer_):
-            win_x = window(self._window_size, X)
-            sub = flattenSegments(win_x)
-            labels = self._clusterer_.predict(sub)
-        else:
-            win_x = window_timeseries(self._window_size, X)
+        if self.overlap:
+            win_x = overlapping_window(self._window_size, self.step_size, X)
             labels = self._clusterer_.predict(win_x)
-        flabel = finalLabels(labels, self._window_size, X)
-        return pd.Series(flabel.flatten(), index=X.index)
+            flabel = overlap_final_label(labels, self._window_size, self.step_size, X)
+            flabel = pd.Series(flabel, index=X.index)
+        else:
+            if is_sklearn_clusterer(self._clusterer_):
+                win_x = window(self._window_size, X)
+                sub = flattenSegments(win_x)
+                labels = self._clusterer_.predict(sub)
+            else:
+                win_x = window_timeseries(self._window_size, X)
+                labels = self._clusterer_.predict(win_x)
+            flabel = finalLabels(labels, self._window_size, X)
+            flabel = pd.Series(flabel.flatten(), index=X.index)
+
+        return flabel
 
     @classmethod
     def get_test_params(cls, parameter_set="default"):
@@ -220,6 +269,30 @@ class WindowSegmenter(BaseSeriesAnnotator):
         params : dict or list of dict, default = {}
 
         """
-        params1 = {"clusterer": KMeans(n_clusters=2), "window_size": 2}
+        params1 = {"clusterer": TimeSeriesKMeans(n_clusters=2), "window_size": 2}
         params2 = {}
         return [params1, params2]
+
+
+n_timepoints = 20
+
+# Generate a sine wave pattern
+time = np.linspace(0, 4 * np.pi, n_timepoints)
+data = np.sin(time)
+
+# Create a pandas Series with a datetime index
+time_index = pd.date_range(start="2024-01-01", periods=n_timepoints, freq="D")
+sine_series = pd.Series(data, index=time_index, name="SineWave")
+
+time1 = np.linspace(0, 4 * np.pi, n_timepoints)
+data1 = np.sin(time)
+
+# Create a pandas Series with a datetime index
+time_index1 = pd.date_range(start="2024-01-01", periods=n_timepoints, freq="D")
+sine_series1 = pd.Series(data1, index=time_index1, name="SineWave1")
+
+
+clus = WindowSegmenter(TimeSeriesKMeans(n_clusters=4), 5, True, 3)
+clus._fit(sine_series)
+ans = clus._predict(sine_series1)
+print(ans)
